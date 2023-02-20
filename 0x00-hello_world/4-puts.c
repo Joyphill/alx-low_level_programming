3153d538708603346355373f89cd5af3cb51d001
joyphill <joyorhohwa@gmail.com>
2023-02-20 15:52:01 -0800

    hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(viod)
+{
+	puts("\"programming is like bulding a multilingual puzzle";
+			return (0);
+}
